3196aaebb6a7d972bec19578b1afb5d9b2f6b571
Rahul Baghel <rahulbaghel6211@gmail.com>
2022-03-21 22:22:16 +0530

    try

---
+++ index.js
@@ -0,0 +1,71 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const app=express();
+
+const connect=()=>{
+
+    return mongoose.connect(
+    "mongodb+srv://dhaval:dhaval_123@cluster0.ljuvz.mongodb.net/web15-atlas?retryWrites=true&w=majority"
+    );
+};
+const bookSchema= new mongoose.Schema({
+    bookName:{type:String,required:true},
+    body:{type:String,required:true},
+})
+const book=mongoose.model("book",bookSchema);
+
+const authorSchema= new mongoose.Schema(
+    {
+firstName:{type:String,required:true},
+lastName:{type:String,required:true}
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    },
+)
+const Schema= new mongoose.Schema(
+    {
+firstName:{type:String,required:true},
+lastName:{type:String,required:true}
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    },
+)
+const author=mongoose.model("author",authorSchema);
+
+app.get("/book",async(req,res)=>{
+
+    try {
+        const book=await book.find().lean().exec();
+        return res.status(200).send({book:book})
+        
+    } catch (error) {
+        return res.status(500).send({message:"worng"})
+        
+    }
+})
+
+app.get("/author",async(req,res)=>{
+
+    try {
+        const author=await author.find().lean().exec();
+        return res.status(200).send({author:author})
+        
+    } catch (error) {
+        return res.status(500).send({message:"worng"})
+        
+    }
+})
+
+app.listen(9989,async()=>{
+    try {
+        await connect();
+        
+    } catch (error) {
+        console.log(error)
+    }
+    console.log("listening on port 9989")
+});
